d66521144b9c10e9bbcb9628a7fc5cb1867fef5a
Austin kabi@ <kabimuriithi@gmail.com>
2024-03-13 13:20:27 +0300

    subtaction of 2 no

---
+++ subtract.py
@@ -0,0 +1,6 @@
+'''a program  to subtract two numbers'''
+#prompt the user for two numbers, subtract the two numbers and display the results
+num1=int(input("Enter first number"))
+num2=int(input("Enter second number"))
+sub=num1-num2
+print("the difference between {} and {} is {}".format(num1,num2,sub))
