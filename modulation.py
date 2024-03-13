9c475fe482b748af0836cfabc878dcef5d9ecba0
Austin kabi@ <kabimuriithi@gmail.com>
2024-03-13 13:31:56 +0300

    modulation

---
+++ modulation.py
@@ -0,0 +1,6 @@
+'''a program to modulate two numbers'''
+#prompt the user for two numbers, modulate the two numbers and display the results
+num1=int(input("Enter first number"))
+num2=int(input("Enter second number"))
+mod=num1%num2
+print("the modulation between {} and {} is {}".format(num1,num2,mod))
