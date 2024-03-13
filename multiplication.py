6cb5a05bdd399d09c4e43958e230a6970f196f9c
Austin kabi@ <kabimuriithi@gmail.com>
2024-03-13 13:46:03 +0300

    multiplication

---
+++ multiplication.py
@@ -0,0 +1,6 @@
+'''a program to multiply two numbers'''
+#prompt the user for two numbers, multiply the two numbers and display the results
+num1=int(input("Enter first number"))
+num2=int(input("Enter second number"))
+multi=num1*num2
+print("the modulation between {} and {} is {}".format(num1,num2,multi))
