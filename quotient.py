4463f09f0781abd2d4b58d02efd0909825f4a214
Austin kabi@ <kabimuriithi@gmail.com>
2024-03-13 13:41:40 +0300

    quotient

---
+++ quotient.py
@@ -0,0 +1,6 @@
+'''a program to quotient two numbers'''
+#prompt the user for two numbers, quotient the two numbers and display the results
+num1=int(input("Enter first number"))
+num2=int(input("Enter second number"))
+quot=num1//num2
+print("the modulation between {} and {} is {}".format(num1,num2,quot))
