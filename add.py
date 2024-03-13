3440ce3056b64f0b5b79e3d06488923c36fde9c2
Austin kabi@ <kabimuriithi@gmail.com>
2024-03-13 13:02:14 +0300

    add two numbers

---
+++ add.py
@@ -0,0 +1,6 @@
+'''' A program to add numbers'''
+#prompt the user for two numbers, add the two numbers and display the results
+num1=int(input("Enter the first number "))
+num2=int(input("Enter second number "))
+sum=num1+num2
+print("The sum of {} and {} is {}".format(num1,num2,sum))
