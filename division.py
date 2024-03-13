49300cb50873458e8af0793fd1e05ce164876497
Austin kabi@ <kabimuriithi@gmail.com>
2024-03-13 13:37:46 +0300

    division

---
+++ division.py
@@ -0,0 +1,6 @@
+'''a program to divide two numbers'''
+#prompt the user for two numbers, divide the two numbers and display the results
+num1=int(input("Enter first number"))
+num2=int(input("Enter second number"))
+div=num1/num2
+print("the division between {} and {} is {}".format(num1,num2,div))
