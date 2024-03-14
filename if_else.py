792cd067b7c5a024409d3e348d47ec1022cbfb59
Austin kabi@ <kabimuriithi@gmail.com>
2024-03-14 13:29:25 +0300

    if_else

---
+++ if_else.py
@@ -0,0 +1,12 @@
+print("what is the answer of 5+5")
+answer=int(input())
+if answer==10:
+    print("correct,5+5=10")
+else:
+    print("5+5 is not equal to{}".format(answer))
+print("is someone eligible to vote")
+age=int(input())
+if age>=18:
+    print("eligible to vote,>=18")
+else:
+    print("age not greater or equal to {} not eligible".format(age))
