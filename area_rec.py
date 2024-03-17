001888f51d5eecc172d413d2ee8b812625858261
Austin kabi@ <kabimuriithi@gmail.com>
2024-03-17 13:17:43 +0300

    area

---
+++ area_rec.py
@@ -0,0 +1,5 @@
+'''a program to find area of rectangles'''
+len=int(input("enter length"))
+wid=int(input("enter width"))
+area=len*wid
+print("the multiplication of {} and {} to give an area of {}".format(len,wid,area))
