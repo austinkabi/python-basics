988d8a45cdd86248bd676e46baf15e1dfc9a1b04
Austin kabi@ <kabimuriithi@gmail.com>
2024-03-18 13:23:30 +0300

    loan

---
+++ loan.py
@@ -0,0 +1,5 @@
+'''a program to calculate time taken to pay a loan'''
+loan=float(input("enter loan"))
+payments=float(input("enter amount payed to bank"))
+TT=loan/payments
+print("time taken to pay is",TT)
